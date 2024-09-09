1b033f5a650f75716279104b8174ac414dfa7e50
Pedro Henrique Silva <146776908+Pedrohcsilva@users.noreply.github.com>
2024-09-09 15:25:02 -0300

    Add files via upload

---
+++ NomeProprio.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class NomeProprio
+{
+    private string nomeCompleto;
+    private string nomePaper;
+
+
+    public NomeProprio(string nomeCompleto)
+    {
+        this.nomeCompleto = nomeCompleto;
+        GerarNomePaper();
+    }
+
+    public NomeProprio(string nomeCompleto, string nomePaper)
+    {
+        this.nomeCompleto = nomeCompleto;
+        this.nomePaper = nomePaper;
+    }
+
+    private void GerarNomePaper()
+    {
+        string[] nomes = nomeCompleto.Split(' ');
+
+        string sobrenome = nomes[nomes.Length - 1];
+
+        string primeiroNome = nomes[0];
+
+        string inicialMeio = "";
+        if (nomes.Length > 2)
+        {
+            inicialMeio = nomes[1];
+            // Garante que a inicial tenha um ponto, mesmo se não houver na entrada
+            if (!inicialMeio.EndsWith("."))
+            {
+                inicialMeio += ".";
+            }
+        }
+        nomePaper = $"{sobrenome}, {primeiroNome} {inicialMeio}";
+    }
+
+    public void ImprimeNomePaper()
+    {
+        Console.WriteLine(nomePaper);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questao1
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o nome: ");
+            string nomeCompleto = Console.ReadLine();
+
+            NomeProprio nome = new NomeProprio(nomeCompleto);
+            nome.ImprimeNomePaper();
+            Console.ReadLine();
+        }
+    }
+}
